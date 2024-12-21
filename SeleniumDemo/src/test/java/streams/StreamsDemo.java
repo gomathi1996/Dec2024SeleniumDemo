bc77368f9a34eb827861755cbf633603a68fe893
gmanohar <30540632+gomathi1996@users.noreply.github.com>
2024-12-21 11:07:55 +0530

    updated streams

--- SeleniumDemo/src/test/java/streams/StreamsDemo.java
+++ SeleniumDemo/src/test/java/streams/StreamsDemo.java
@@ -2,7 +2,14 @@ package streams;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import dev.failsafe.internal.util.Assert;
@@ -101,13 +108,68 @@ public class StreamsDemo {
 
 	}
 	
+	public void streamCollect() {
+		List<Integer> list = Arrays.asList(1,4,2,6,7,1,3,2,6,7,4);
+		
+		//sort an array
+		List<Integer> sortedList = list.stream().sorted().collect(Collectors.toList());
+		System.out.println(sortedList);
+		
+		//print unique number
+		list.stream().distinct().sorted().forEach(s->System.out.println(s));
+		
+		Map<Integer, Long> result = list.stream().collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
+		System.out.println(result);
+		
+		Map<Integer, Integer> result1 = list.stream().collect(Collectors.groupingBy(Function.identity(),
+                Collectors.summingInt(e -> 1)));
+		System.out.println(result1);
+		
+	}
+	
+	public void collectionFreq() {
+		List<Integer> list = Arrays.asList(1,4,2,6,7,1,3,2,6,7,4,1,4,4);
+		Map<Integer, Integer> result = new HashMap<Integer, Integer>();
+		for(int i =0; i<list.size();i++) {
+			int freq = Collections.frequency(list, list.get(i));
+			result.put(list.get(i), freq);
+			
+		}
+		// Get keys and values
+//		for(Map.Entry<Integer, Integer> entry: result.entrySet()) {
+//			int key = entry.getKey();
+//			int val = entry.getValue();
+//			System.out.println(key+" : "+val);
+//			
+//		}
+		
+		//Java 8
+//		result.forEach((k,v)->System.out.println(k + " - "+v));
+		
+		//get all keys
+//		Set<Integer> keys = result.keySet();
+//		for(Integer k: keys) {
+//			System.out.println(k);
+//		}
+		
+		//get all values
+		Collection<Integer> values = result.values();
+		for(Integer val:values) {
+			System.out.println(val);
+		}
+		
+		
+	}
+	
 	public static void main(String[] args) {
 		StreamsDemo sd = new StreamsDemo();
 //		sd.regularway();
 //		sd.usingStream();
 //		sd.printUsingStream();
 //		sd.streammap();
-		sd.streamMerge();
+//		sd.streamMerge();
+//		sd.streamCollect();
+		sd.collectionFreq();
 
 	}
 
